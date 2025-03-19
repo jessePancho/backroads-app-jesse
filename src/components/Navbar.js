aad72213a9c0c571219019e6816d1f65c879cc33
jessePancho <juicepancho007@gmail.com>
2025-03-19 15:04:55 +0800

    update: footer and removed unnecessary fragments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,13 +29,11 @@ const Navbar = () => {
           {socialLinks.map((link) => {
             const { href, icon, id } = link;
             return (
-              <>
-                <li key={id}>
-                  <a href={href} target="_blank" className="nav-icon">
-                    <i className={icon}></i>
-                  </a>
-                </li>
-              </>
+              <li key={id}>
+                <a href={href} target="_blank" className="nav-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
             );
           })}
         </ul>
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,17 +11,15 @@ const Services = () => {
           {services.map((service) => {
             const { icon, title, text, id } = service;
             return (
-              <>
-                <article className="service" key={id}>
-                  <span className="service-icon">
-                    <i className={icon}></i>
-                  </span>
-                  <div className="service-info">
-                    <h4 className="service-title">{title}</h4>
-                    <p className="service-text">{text}</p>
-                  </div>
-                </article>
-              </>
+              <article className="service" key={id}>
+                <span className="service-icon">
+                  <i className={icon}></i>
+                </span>
+                <div className="service-info">
+                  <h4 className="service-title">{title}</h4>
+                  <p className="service-text">{text}</p>
+                </div>
+              </article>
             );
           })}
         </div>
