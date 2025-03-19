554befde414cd3c0a883e61af00596497b1cfabd
jessePancho <juicepancho007@gmail.com>
2025-03-19 14:13:37 +0800

    update: Tours dynamic data

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,39 +1,45 @@
 import React from "react";
 import Title from "./Title";
+import { tours } from "../data";
 
 const Tours = () => {
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
       <div className="section-center featured-center">
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">august 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>6 days</p>
-              <p>from $2100</p>
-            </div>
-          </div>
-        </article>
+        {tours.map((tour) => {
+          const { id, image, date, title, info, location, duration, cost } =
+            tour;
 
-        <article className="tour-card">
+          return (
+            <>
+              <article className="tour-card" key={id}>
+                <div className="tour-img-container">
+                  <img src={image} className="tour-img" alt={title} />
+                  <p className="tour-date">{date}</p>
+                </div>
+                <div className="tour-info">
+                  <div className="tour-title">
+                    <h4>{title}</h4>
+                  </div>
+                  <p>{info}</p>
+                  <div className="tour-footer">
+                    <p>
+                      <span>
+                        <i className="fas fa-map"></i>
+                      </span>{" "}
+                      {location}
+                    </p>
+                    <p>{duration} days</p>
+                    <p>from {cost}</p>
+                  </div>
+                </div>
+              </article>
+            </>
+          );
+        })}
+
+        {/* <article className="tour-card">
           <div className="tour-img-container">
             <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
             <p className="tour-date">october 1th, 2020</p>
@@ -106,7 +112,7 @@ const Tours = () => {
               <p>from $3300</p>
             </div>
           </div>
-        </article>
+        </article> */}
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   {
     id: 1,
@@ -57,5 +62,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "October 11th, 2020",
+    title: "Best of Java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Indonesia",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "September 15th, 2020",
+    title: "Explore Hong Kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Hong Kong",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "December 5th,",
+    title: "Kenya Highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Kenya",
+    duration: 6,
+    cost: 2100,
   },
 ];
